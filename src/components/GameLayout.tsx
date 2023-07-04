8dce763c8a4f4bbfa9133fb097deea14c2a9daae
mmorenoa <manuelmorenoasiain@gmail.com>
2023-07-05 00:59:44 +0200

    Corregido error que impedia el despliegue

--- src/components/GameLayout.tsx
+++ src/components/GameLayout.tsx
@@ -5,7 +5,7 @@ const GameLayout = () => {
 
   return (
     <div className="grid grid-rows-5 gap-2 w-full h-[75vh] md:h-[65vh] 3xl:h-[50vh] p-5 xl:p-16">
-      {[...Array(NUMBER_OF_TRIES)].map((e, index) => (
+      {[...Array(NUMBER_OF_TRIES)].map((_e, index) => (
         <Row key={index} index={index} />
       ))}
     </div>
